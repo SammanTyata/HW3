d63e27ab41e9848b7c4e5d6d280489556e071774
Samman <tyatasamman@gmail.com>
2024-11-03 19:15:28 -0800

    Initial Commit

---
+++ HW3/HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3App.swift
+//  HW3
+//
+//  Created by Samman Tyata on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
